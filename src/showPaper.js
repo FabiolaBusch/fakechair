5bc452c46e0a8eb450d1782daf3b9ec469fe73bf
f.buschendorf <f.buschendorf@stud.uni-goettingen.de>
2018-06-06 17:16:01 +0200

    Dirty fix: paperLength 0

--- src/showPaper.js
+++ src/showPaper.js
@@ -60,9 +60,11 @@ class ShowPaper extends React.Component{
     const conferenceInstance = await conference.at(this.state.conferenceAddress)
 
     const paperLength = await conferenceInstance.getPaperLength({from: accounts[0]})
+    this.setState({paperLength: 0})
     const confTitle = await conferenceInstance.title({from: accounts[0]})
     const cleanConfTitle = cleanTitle(confTitle)
     this.setState({paperLength, cleanConfTitle})
+
 
     const cleanTitles = [];
     const hashes = [];
